bdf19d26ca8b44ca4d3aab95ab33c9e7e446d813
= <trevis.wamuthenya@qhala.com>
2023-04-13 12:00:45 +0300

    init

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+  <iframe
+    width="460"
+    height="315"
+    :src="`https://player.vimeo.com/video/${props.videoId}`"
+    title="Video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  videoId: {
+    type: Number,
+    required: true
+  }
+})
+</script>
+
+
+<style scoped></style>
